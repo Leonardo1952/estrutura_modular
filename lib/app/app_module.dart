0b5d5aedb436e17af43d6aaa5df6b70ada653a16
Leonardo1952 <lo88980@gmail.com>
2021-01-20 15:00:44 -0300

    salvamento de tema

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,5 +1,7 @@
 import 'package:estrutura_modular/app/app_controler.dart';
 import 'package:estrutura_modular/app/app_widget.dart';
+import 'package:estrutura_modular/app/core/interfaces/shared_repository_interface.dart';
+import 'package:estrutura_modular/app/core/repositories/shared_repository.dart';
 import 'package:estrutura_modular/app/modules/home/home_module.dart';
 import 'package:estrutura_modular/app/modules/splash/splash_page.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -8,7 +10,14 @@ import 'package:flutter_modular/flutter_modular.dart';
 class AppModule extends MainModule {
   @override
   // Este é para a injeção de dependencia
-  List<Bind> get binds => [Bind((i) => AppController())];
+  List<Bind> get binds => [
+        Bind<ISharedRepositoryInterface>(
+          (i) => SharedRepository(),
+        ),
+        Bind(
+          (i) => AppController(),
+        ),
+      ];
 
   @override
   // Primeira aplicação a ser iniciada
@@ -17,9 +26,9 @@ class AppModule extends MainModule {
   @override
   // Rotas nomeadas
   List<ModularRouter> get routers => [
-        ModularRouter(
-          '/', child: (_, args) => SplashPage(),
-        transition: TransitionType.noTransition),
+        ModularRouter('/',
+            child: (_, args) => SplashPage(),
+            transition: TransitionType.noTransition),
         ModularRouter(
           '/home',
           module: HomeModule(),
